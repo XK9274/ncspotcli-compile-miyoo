f9034dd60bb74635748ff0ba095d6517af5dda98
XK <47260768+XK9274@users.noreply.github.com>
2023-05-14 15:33:49 +0100

    New audio bridge, removed kill for audioserver, kill NTP or it breaks audio stream.

--- App/ncspot/spotlaunch.sh
+++ App/ncspot/spotlaunch.sh
@@ -4,14 +4,22 @@ export miyoodir=/mnt/SDCARD/miyoo
 export LD_LIBRARY_PATH="$sysdir/ncspot/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"				   
 
 #set time and date to a time when ncspot certs were valid or you'll get errors saying they dont exist yet, or aren't valid.. the time doesn't matter
-export TZ=UTC+1
-ntpd -p time.google.com
+if pgrep "ntpd" > /dev/null
+then
+    echo "NTPD already running"
+	echo "Connecting to Spotify..."
+else   
+	echo "Trying to set time and date for the spotify certificate check"
+    export TZ=UTC+1
+    ntpd -p time.google.com
+	echo "Connecting to Spotify..."
+fi
 
 #Setup some env vars
 export XDG_CONFIG_HOME="/tmp"
 export XDG_CACHE_HOME="/tmp"
 
-killall -9 audioserver 
+killall -9 ntpd # Timne is possibly breaking playback as its never 100% in syncx
 
 # some cleanup
 if pgrep "audiobridge" >/dev/null; then
@@ -20,4 +28,4 @@ else
     echo ""
 fi
 
-/mnt/SDCARD/App/ncspot/bin/ncspot -c /mnt/SDCARD/App/ncspot/config/config.toml
+LD_PRELOAD=libpadsp.so /mnt/SDCARD/App/ncspot/bin/ncspot -c /mnt/SDCARD/App/ncspot/config/config.toml
