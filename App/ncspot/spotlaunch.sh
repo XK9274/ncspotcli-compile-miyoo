9bd3cb3012945044025c259b22074c8eb29b7915
XK <47260768+XK9274@users.noreply.github.com>
2023-05-21 22:33:20 +0100

    Minor changes

--- App/ncspot/spotlaunch.sh
+++ App/ncspot/spotlaunch.sh
@@ -1,23 +1,14 @@
 #set lib locs etc
 export sysdir=/mnt/SDCARD/App
 export miyoodir=/mnt/SDCARD/miyoo
-export LD_LIBRARY_PATH="$sysdir/ncspot/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"				   
+export LD_LIBRARY_PATH="$sysdir/ncspot/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
 
 #set time and date to a time when ncspot certs were valid or you'll get errors saying they dont exist yet, or aren't valid.. the time doesn't matter
-if pgrep "ntpd" > /dev/null
-then
-    echo "NTPD already running"
-else   
-	echo "Trying to set time and date for the spotify certificate check"
-    export TZ=UTC+1
-    ntpd -p time.google.com
-fi
+date -s 2023.05.15-00:00
 
 #Setup some env vars
 export XDG_CONFIG_HOME="/tmp"
 export XDG_CACHE_HOME="/tmp"
-
-killall -9 ntpd # Timne is possibly breaking playback as its never 100% in syncx
 
 # some cleanup
 if pgrep "audiobridge" >/dev/null; then
@@ -26,4 +17,4 @@ else
     echo ""
 fi
 
-LD_PRELOAD=libpadsp.so /mnt/SDCARD/App/ncspot/bin/ncspot -c /mnt/SDCARD/App/ncspot/config/config.toml
+LD_PRELOAD=/customer/lib/libpadsp.so /mnt/SDCARD/App/ncspot/bin/ncspot -c /mnt/SDCARD/App/ncspot/config/config.toml
