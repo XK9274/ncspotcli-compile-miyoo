b5c536636f311f110bbe3c80a3df5a53d77b94a6
XK <47260768+XK9274@users.noreply.github.com>
2023-09-24 16:26:08 +0100

    Update splash to animated, launch.sh, add build script.

--- App/ncspot/launch.sh
+++ App/ncspot/launch.sh
@@ -3,11 +3,26 @@ sysdir=/mnt/SDCARD/.tmp_update
 miyoodir=/mnt/SDCARD/miyoo
 export LD_LIBRARY_PATH="/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte:$(dirname "$0")/lib"
 export PATH="$sysdir/bin:$PATH"
-/mnt/SDCARD/App/ncspot/bin/splash /mnt/SDCARD/App/ncspot/logos/splash.png 60 60 60 &
-splash_pid=$!
-sleep 1
-kill -2 $splash_pid
-sleep 2
-killall -9 splash
+
+sync
+
+#copies from persistent if exists
+if [ ! -d "/tmp/ncspot/librespot" ]; then
+    mkdir -p /tmp/ncspot/librespot
+fi
+
+if [ -f "/mnt/SDCARD/App/ncspot/config/credentials.json" ]; then
+    cp -f /mnt/SDCARD/App/ncspot/config/credentials.json /tmp/ncspot/librespot/
+fi
+
+/mnt/SDCARD/App/ncspot/bin/rapid-splash /mnt/SDCARD/App/ncspot/logos/frames 0 0 0 50
+
+killall -9 rapid-splash
+
 /mnt/SDCARD/.tmp_update/bin/st -e sh /mnt/SDCARD/App/ncspot/spotlaunch.sh
+
+# copies to persistent if exists, comment this out to freeze changes to your "permanent" file
+if [ -f "/tmp/ncspot/librespot/credentials.json" ]; then
+    cp -f /tmp/ncspot/librespot/credentials.json /mnt/SDCARD/App/ncspot/config/
+fi
 
