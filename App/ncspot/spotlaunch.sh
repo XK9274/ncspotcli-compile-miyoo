00c047654c333f71a300fea8c7fec6e5d4b2dff0
XK <47260768+XK9274@users.noreply.github.com>
2024-03-19 17:42:39 +0000

    Update spotlaunch.sh

--- App/ncspot/spotlaunch.sh
+++ App/ncspot/spotlaunch.sh
@@ -4,7 +4,7 @@ export miyoodir=/mnt/SDCARD/miyoo
 export LD_LIBRARY_PATH="$sysdir/ncspot/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
 
 #set time and date to a time when ncspot certs were valid or you'll get errors saying they dont exist yet, or aren't valid.. the time doesn't matter
-date -s '2023.05.15-00:00' > /dev/null 2>&1
+date -s '2024.03.15-00:00' > /dev/null 2>&1
 
 #Setup some env vars
 export XDG_CONFIG_HOME="/tmp"
